225b0ed0c63e063ad88862d32ed056c980bb36e0
d3ud3u-ntp <168897397+d3ud3u-ntp@users.noreply.github.com>
2024-05-12 21:24:19 +0900

    フォントが読み込めないとのことで、確実に有効なフォントを読み込ませたが、うまく読み込んでくれない

--- 20240512_chromeext_imagefilter_ksms/README.md
+++ 20240512_chromeext_imagefilter_ksms/README.md
@@ -1,3 +1,11 @@
 # Chrome Extensionで画像にくそみそフィルターをかける
 
 - Chrome Extension のネタが降ってきたので、できるかわからんがつくる
+
+ChromeExtensionで以下のプログラムを実現したいです。
+・画像の色を点でランダムで取得して、その点を中心として、「A」という文字を画像に描画。この処理は100回行われる
+・abea.ttfというオリジナルフォントの「A」という文字を描画する
+・ページ内の画像にすべて加工を施す。
+
+JavaScriptでloadを使わずにFontFaceを読み込む方法
+ChromeExtensionでFontFaceを読み込む方法を3つ
--- 20240512_chromeext_imagefilter_ksms/TheFirstExtension/js/content.js
+++ 20240512_chromeext_imagefilter_ksms/TheFirstExtension/js/content.js
@@ -1,33 +1,46 @@
+console.log("load");
 // ページ内の全ての画像要素を取得
 const images = document.querySelectorAll('img');
 
+const canvas = document.createElement('canvas');
+const ctx = canvas.getContext('2d');
+
+// フォントをロードする
+// const font = new FontFace('abea', 'url(chrome-extension://__MSG_@@extension_id__/abea.ttf)');
+const font = new FontFace('abea', 'url(chrome-extension://__MSG_@@extension_id__/aboutaxmas.ttf)');
+document.fonts.add(font);
+// フォントが読み込まれたことを確認
 // 各画像要素に対して処理を行う
 for (const img of images) {
   // 画像が読み込まれた後に処理を実行
 //   img.addEventListener('load', () => {
-    const canvas = document.createElement('canvas');
-    const ctx = canvas.getContext('2d');
     canvas.width = img.width;
     canvas.height = img.height;
     ctx.drawImage(img, 0, 0);
+    
+    font.load().then(loadedFont => {
+      document.fonts.add(loadedFont);
+      ctx.font = '100px about a Xmas';
 
-    // ランダムな点を100回選択し、その点の色を取得して「A」を描画
-    for (let i = 0; i < 100; i++) {
-      const x = Math.floor(Math.random() * canvas.width);
-      const y = Math.floor(Math.random() * canvas.height);
-      console.log("x="+x);
-      console.log("y="+y);
-      const pixelData = ctx.getImageData(x, y, 1, 1).data;
-      console.log("pixel="+pixelData);
-      const r = pixelData[0];
-      const g = pixelData[1];
-      const b = pixelData[2];
-      const color = `rgb(${r}, ${g}, ${b})`;
+      // ランダムな点を100回選択し、その点の色を取得して「A」を描画
+      for (let i = 0; i < 100; i++) {
+        const x = Math.floor(Math.random() * canvas.width);
+        const y = Math.floor(Math.random() * canvas.height);
+        console.log("x="+x);
+        console.log("y="+y);
+        const pixelData = ctx.getImageData(x, y, 1, 1).data;
+        console.log("pixel="+pixelData);
+        const r = pixelData[0];
+        const g = pixelData[1];
+        const b = pixelData[2];
+        const color = `rgb(${r}, ${g}, ${b})`;
 
-      ctx.font = '20px Arial';
-      ctx.fillStyle = color;
-      ctx.fillText('A', x, y);
-    }
+        //   ctx.font = '20px Arial';
+        ctx.fillStyle = color;
+        ctx.fillText('A', x, y);
+      }
+    });
+    
 
     // 処理後の画像をページに反映
     const processedImage = new Image();
